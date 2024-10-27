bfd9e80464a1420a7419db25efc9161b9bfb36f4
Auth0r <osafune0327dk@gmail.com>
2024-10-27 23:26:08 +0900

    add

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh -e
+
+IGNORE_PATTERN="^\.(git|travis)"
+
+echo "Create dotfile links."
+for dotfile in .??*; do
+    [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
+    ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
+done
+echo "Success"
+
