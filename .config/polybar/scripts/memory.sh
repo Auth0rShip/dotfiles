db9b6b6ae7fdc60cfae9973e56ed87aedd4a5daf
Auth0r <osafune0327dk@gmail.com>
2024-11-26 01:48:27 +0900

    add

---
+++ .config/polybar/scripts/memory.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+mem_used=$(free -m | sed -n 2P | awk '{print $3}')
+mem_used_2=$(echo "scale=1; $mem_used / 1024" | bc)
+
+if [[ $1 = click ]]; then
+    if [ ${mem_used_2::1} = "." ]; then
+        notify-send "MEM: 0${mem_used_2}GB"
+    else
+        notify-send "MEM: ${mem_used_2}GB"
+    fi
+else
+    echo "AAA"
+fi
