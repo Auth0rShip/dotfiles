6876fef6b1da69407fb5ba704c1d734616a39836
Auth0r <osafune0327dk@gmail.com>
2025-04-12 10:11:15 +0900

    fix

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,7 +5,7 @@
 #polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 
-## restart i3
+# restart i3
 if [[ $1 = "restart" ]]; then
 	i3-msg restart
 	sleep 1
@@ -15,7 +15,17 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar 2>&1 | tee -a /tmp/polybar1.log & disown
+#polybar 2>&1 | tee -a /tmp/polybar1.log & disown
 #polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+MONITOR="DP-2" polybar --reload statusbar & MONITOR="DP-0" polybar --reload statusbar
+
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload statusbar &
+#  done
+#else
+#	polybar --reload example &
+#fi
 
 echo "Bars launched..."
